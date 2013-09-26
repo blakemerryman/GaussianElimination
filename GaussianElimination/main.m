48ed7c1e7580030fb31c3f62e44fc3fb74e6c73b
blakemerryman <blake.merryman@gmail.com>
2013-09-25 23:16:29 -0500

    Initial commit: just playing with basic 2-dimensional C-arrays.

--- GaussianElimination/main.m
+++ GaussianElimination/main.m
@@ -10,13 +10,25 @@
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"\nStart Program!\n");
         
-    }
+        int matrix[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+        
+        for (int i = 0; i < 3; i++)
+        {
+            printf("| ");
+            for (int j = 0; j < 3; j++)
+            {
+                printf("%i  ", matrix[i][j]);
+            }
+            printf("|");
+            printf("\n");
+        }
+        
+    } // END MEMORY MANAGEMENT BLOCK.
     return 0;
 }
 
