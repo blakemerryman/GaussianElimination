52864c3e614b9f698d92edea31f48aabdc4fe092
blakemerryman <blake.merryman@gmail.com>
2013-09-26 12:14:18 -0500

    Starting fresh.

--- GaussianElimination/main.m
+++ GaussianElimination/main.m
@@ -8,31 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
-void testFunction(void)
-{
-    // insert code here...
-    NSLog(@"\nStart Program!\n");
-    
-    int matrix[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
-    
-    for (int i = 0; i < 3; i++)
-    {
-        printf("| ");
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%i  ", matrix[i][j]);
-        }
-        printf("|");
-        printf("\n");
-    }
-}
+//void testFunction(void)
+//{
+//    // insert code here...
+//    NSLog(@"\nStart Program!\n");
+//    
+//    int matrix[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+//    
+//    for (int i = 0; i < 3; i++)
+//    {
+//        printf("| ");
+//        for (int j = 0; j < 3; j++)
+//        {
+//            printf("%i  ", matrix[i][j]);
+//        }
+//        printf("|");
+//        printf("\n");
+//    }
+//}
 
 int main(int argc, const char * argv[])
 {
     @autoreleasepool
     { // BEGIN MEMORY MANAGEMENT BLOCK.
         
-        testFunction();
+        //testFunction();
         
         
     
