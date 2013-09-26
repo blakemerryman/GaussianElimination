756af89be5438e0a6b917e726544bf00a2fb5467
blakemerryman <blake.merryman@gmail.com>
2013-09-25 21:13:23 -0500

    Initial Commit

---
+++ GaussianElimination/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GaussianElimination
+//
+//  Created by Blake Merryman on 9/25/13.
+//  Copyright (c) 2013 Blake Merryman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
