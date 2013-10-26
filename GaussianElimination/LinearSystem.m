7d9b8017525680f6df8571003dca264a94ccabe5
blakemerryman <blake.merryman@gmail.com>
2013-10-26 08:46:50 -0500

    Fixed initial issues of not solving properly:  * Added method to swaps rows in B when swapping in A.  * Checked to make sure the values that checked for zero were checking with fabs(value) to allow for negative values to pass test.  * refactored code for readability & debuggability.

--- GaussianElimination/LinearSystem.m
+++ GaussianElimination/LinearSystem.m
@@ -17,7 +17,7 @@
 #import "LinearSystem.h"
 
 #pragma mark - Private Interface Method Declarations
-@interface LinearSystem (PrivateMethods)                   // This is the private method interface for LinearSystem object.
+@interface LinearSystem (PrivateMethods)                   // Private method interface for LinearSystem object.
 
 -(void)ScaleRowsByLargestAbsoluteValueInRow;               // Scales a row based upon the largest value in row.
 -(NSUInteger)FindPivotElementForStep:(NSUInteger)step;     // Finds the location of the largest possible pivot element.
@@ -94,7 +94,8 @@
                 [rowOfValues addObject: [NSNumber numberWithDouble:rowElement]];
             }
             // Adds row of values to the matrix B.
-            [_matrixA insertObject:rowOfValues atIndex:(row-1)];
+            [_matrixA insertObject:rowOfValues
+                           atIndex:(row-1)];
             
             // Repurposes rowElement to retrieve & store the values for B in the same manner as for the rowOfValues array.
             rowElement = [[[[[stringContents componentsSeparatedByString:@"\n"]objectAtIndex:(_n+1)]componentsSeparatedByString:@" "]objectAtIndex:(row-1)]doubleValue];
@@ -125,8 +126,13 @@
         // Returns the row index of best possible pivot location.
         pivotElementRowIndexForCurrentStep = [self FindPivotElementForStep:step];
         
-        // Framework provided method that swaps the object indices (instead of object contents).
-        [_matrixA exchangeObjectAtIndex:step withObjectAtIndex:pivotElementRowIndexForCurrentStep];
+        if (pivotElementRowIndexForCurrentStep != step)
+        {
+            // Framework provided method that swaps the object indices (instead of object contents).
+            // Swap rows in matrix A then swap rows in matrix B.
+            [_matrixA exchangeObjectAtIndex:step withObjectAtIndex:pivotElementRowIndexForCurrentStep];
+            [_matrixB exchangeObjectAtIndex:step withObjectAtIndex:pivotElementRowIndexForCurrentStep];
+        }
         
         // Reduce rows at current step.
         [self ReduceRowsForStep:step];
@@ -142,17 +148,15 @@
  */
 -(void)SaveSolutionToFile
 {
-    //[_matrixB writeToFile:@"/Users/blakemerryman/Desktop/solution.txt" atomically:NO];
     NSString* writeContentsToFileName = @"/Users/blakemerryman/Desktop/solution.txt";
-    
-    // Allocates & initializes a string that will hold our solution values.
-    NSString* stringToSave = [[NSString alloc] init];
+    NSString* stringToSave = [[NSString alloc] init];   // Allocates & initializes a string that will hold our solution values.
     
     // Loops through the solution values...
     for (NSUInteger colIndex = 0; colIndex < _n; colIndex++)
     {
         // Converts each solution's double value to formatted string values. Decimal places are given by the .# in the string.
-        [_matrixB replaceObjectAtIndex:colIndex withObject:[NSString stringWithFormat:@"%.4f",[[_matrixB objectAtIndex:colIndex] doubleValue]]];
+        [_matrixB replaceObjectAtIndex:colIndex
+                            withObject:[NSString stringWithFormat:@"%.4f",[[_matrixB objectAtIndex:colIndex] doubleValue]]];
         
         // Appends the formatted solution value to the string to string to be saved. Seperates each by a newline character.
         stringToSave = [stringToSave stringByAppendingFormat:@"%@\n",[_matrixB objectAtIndex:colIndex]];
@@ -213,8 +217,10 @@
             /* FOR MATRIX A (current row) */
             // Calculates the newly scaled value at current column index for row.
             temporaryValueInRow = [[[_matrixA objectAtIndex:rowIndex] objectAtIndex:colIndex] doubleValue] * scalingFactor;
+            
             // Replaces the original value with newly scaled value at given current column index for row.
-            [[_matrixA objectAtIndex:rowIndex] replaceObjectAtIndex:colIndex withObject:[NSNumber numberWithDouble:temporaryValueInRow]];
+            [[_matrixA objectAtIndex:rowIndex] replaceObjectAtIndex:colIndex
+                                                         withObject:[NSNumber numberWithDouble:temporaryValueInRow]];
         }
 
         /* FOR MATRIX B (current row) */
@@ -222,7 +228,8 @@
         temporaryValueInRow = [[_matrixB objectAtIndex:rowIndex] doubleValue] * scalingFactor;
         
         // Replaces the original value with newly scaled value at current row index for row.
-        [_matrixB replaceObjectAtIndex:rowIndex withObject:[NSNumber numberWithDouble:temporaryValueInRow]];
+        [_matrixB replaceObjectAtIndex:rowIndex
+                            withObject:[NSNumber numberWithDouble:temporaryValueInRow]];
     }
     
 }
@@ -274,20 +281,22 @@
     
     for (NSUInteger row = (step+1); row < _n; row ++)
     {
-        currentRowElement = [[[_matrixA objectAtIndex:row]objectAtIndex:step]doubleValue];      // The current row's value beneath the pivot.
-        pivotRowElement   = [[[_matrixA objectAtIndex:step]objectAtIndex:step]doubleValue];     // The current step's value for the pivot element.
-        double multiplier = currentRowElement / pivotRowElement;                                // Calculates multiplier for row reduction.
+        currentRowElement = [[[_matrixA objectAtIndex:row] objectAtIndex:step] doubleValue];      // The current row's value beneath the pivot.
+        pivotRowElement   = [[[_matrixA objectAtIndex:step] objectAtIndex:step] doubleValue];     // The current step's value for the pivot element.
+        double multiplier = currentRowElement / pivotRowElement;                                  // Calculates multiplier for row reduction.
+       
         [[_matrixA objectAtIndex:row] replaceObjectAtIndex:step
-                                                withObject:[NSNumber numberWithDouble:0.0]];    // Fill in value below pivot with zero for current row.
+                                                withObject:[NSNumber numberWithDouble:0.0]];      // Fill in value below pivot with zero for current row.
         
         // Loops through the remaining elements in the row...
         for (NSUInteger col = (step+1); col < _n; col++)
         {
-            double currentRowElement = [[[_matrixA objectAtIndex:row]objectAtIndex:col]doubleValue];    // The current row's element for current column
-            double pivotRowElement   = [[[_matrixA objectAtIndex:step]objectAtIndex:col]doubleValue];   // The pivot row's element for current column.
-            newValue = currentRowElement - multiplier * pivotRowElement;                                // Calculates new row-reduced value for current element.
+            currentRowElement = [[[_matrixA objectAtIndex:row] objectAtIndex:col] doubleValue];        // The current row's element for current column
+            pivotRowElement   = [[[_matrixA objectAtIndex:step] objectAtIndex:col] doubleValue];       // The pivot row's element for current column.
+            newValue = currentRowElement - multiplier * pivotRowElement;                               // Calculates new row-reduced value for current element.
+            
             [[_matrixA objectAtIndex:row]replaceObjectAtIndex:col
-                                                   withObject:[NSNumber numberWithDouble:newValue]];    // Replaces the current element with the new value.
+                                                   withObject:[NSNumber numberWithDouble:newValue]]; // Replaces the current element with the new value.
         }
         
         currentRowElement = [[_matrixB objectAtIndex:row]doubleValue];           // The current row's b value.
@@ -304,19 +313,24 @@
  */
 -(void)BackSubstitution
 {
-    // SAFETY: If the diagonal term for this row is considered "zero"
-    double nXnTerm = fabs( [[[_matrixA objectAtIndex:(_n-1)]objectAtIndex:(_n-1)]doubleValue] );
-    if ( nXnTerm < _LS_ZERO_THRESHOLD)
+    //
+    double stepElement = [[[_matrixA objectAtIndex:(_n-1)]objectAtIndex:(_n-1)]doubleValue];
+    
+    // SAFETY: If the diagonal term for this row is considered "zero"...
+    if ( fabs(stepElement) < _LS_ZERO_THRESHOLD)
     {
         // Display error message to terminal and terminate the program.
         NSLog(@"ERROR: Attempted to divide by zero during back substitution on the initial back-step"); exit(0);
     }
     
     // Creates & initializes the X value to be used throughout for calculations.
-    double xValue = [[_matrixB objectAtIndex:(_n-1)]doubleValue] / [[[_matrixA objectAtIndex:(_n-1)]objectAtIndex:(_n-1)]doubleValue];
+    double bValue = [[_matrixB objectAtIndex:(_n-1)]doubleValue];
+    double aValue = [[[_matrixA objectAtIndex:(_n-1)]objectAtIndex:(_n-1)]doubleValue];
+    double xValue = bValue / aValue;
     
     // Wraps double value in NSNumber object wrapper & begins replacing the values in matrix B starting with the last value.
-    [_matrixB replaceObjectAtIndex:(_n-1) withObject:[NSNumber numberWithDouble:xValue]];
+    [_matrixB replaceObjectAtIndex:(_n-1)
+                        withObject:[NSNumber numberWithDouble:xValue]];
     
     // Loops backward through the rest of the linear system to solve for the remain X-values.
     for (NSInteger rowIndex = (_n-2); rowIndex >= 0; rowIndex--)
@@ -330,17 +344,20 @@
             rowSum += [[[_matrixA objectAtIndex:rowIndex]objectAtIndex:colIndex]doubleValue] * xValue;
         }
         
+        //
+        stepElement = [[[_matrixA objectAtIndex:rowIndex]objectAtIndex:rowIndex]doubleValue];
+        
         // SAFETY: If the diagonal term for this row is considered "zero"
-        double nXnTerm = fabs( [[[_matrixA objectAtIndex:rowIndex]objectAtIndex:rowIndex]doubleValue] );
-
-        if ( nXnTerm < _LS_ZERO_THRESHOLD )
+        if ( fabs(stepElement) < _LS_ZERO_THRESHOLD )
         {
             // Display error message to terminal and terminate the program.
             NSLog(@"ERROR: Attempted to divide by zero during back substitution on the %lith step!",(long)rowIndex+1); exit(0);
         }
         
         // Calculates new x value for this row.
-        xValue = ([[_matrixB objectAtIndex:rowIndex]doubleValue] - rowSum) / [[[_matrixA objectAtIndex:rowIndex]objectAtIndex:rowIndex]doubleValue];
+        bValue = [[_matrixB objectAtIndex:rowIndex]doubleValue];
+        aValue = [[[_matrixA objectAtIndex:rowIndex]objectAtIndex:rowIndex]doubleValue];
+        xValue = (bValue - rowSum) / aValue;
         
         // Wraps double value in NSNumber object wrapper & continues replacing the values of matrix B from last to first.
         [_matrixB replaceObjectAtIndex:rowIndex withObject:[NSNumber numberWithDouble:xValue]];
